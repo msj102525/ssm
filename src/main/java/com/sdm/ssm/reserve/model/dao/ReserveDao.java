9713c0a49e62f5a3c147fa0901fff353681c6f66
ict01-29 <tactius90@gmail.com>
2024-03-28 17:33:45 +0900

    예약관리 등록(작업중) / 2024-03-28/ 남길현

--- src/main/java/com/sdm/ssm/reserve/model/dao/ReserveDao.java
+++ src/main/java/com/sdm/ssm/reserve/model/dao/ReserveDao.java
@@ -1,31 +1,35 @@
 package com.sdm.ssm.reserve.model.dao;
 
 import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.sdm.ssm.reserve.controller.ReserveController;
 import com.sdm.ssm.reserve.model.vo.Reserve;
 
 @Repository("reserveDao")
 public class ReserveDao {
-	
+	private static final Logger logger = LoggerFactory.getLogger(ReserveDao.class);
+
 	@Autowired //root-context.xml 에서 생성한 객체를 자동 연결함
 	private SqlSessionTemplate sqlSessionTemplate;
 
 	public int insertReserve(Reserve reserve) {
-		return sqlSessionTemplate.insert("mapper.insertReserve", reserve);
+		return sqlSessionTemplate.insert("reserveMapper.insertReserve", reserve);
 	}
 
 	public int updateReserve(Reserve reserve) {
-		return sqlSessionTemplate.update("mapper.updateReserve", reserve);
+		return sqlSessionTemplate.update("reserveMapper.updateReserve", reserve);
 	}
 
 	public int deleteReserve(Reserve reserve) {
-		return sqlSessionTemplate.delete("mapper.deleteReserve", reserve);
+		return sqlSessionTemplate.delete("reserveMapper.deleteReserve", reserve);
 	}
 
 	public Reserve selectRsrvDetail(Reserve reserve) {
-		return sqlSessionTemplate.selectOne("mapper.selectRsrvDetail", reserve);
+		return sqlSessionTemplate.selectOne("reserveMapper.selectRsrvDetail", reserve);
 	}
 
 }
