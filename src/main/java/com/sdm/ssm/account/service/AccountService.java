51708ef327368c8f549d543685da74825a0c9f7e
tpals213 <tpals213@gmail.com>
2024-04-09 13:19:43 +0900

    발주내역 연동/ 메인페이지 연동

---
+++ src/main/java/com/sdm/ssm/account/service/AccountService.java
@@ -0,0 +1,19 @@
+package com.sdm.ssm.account.service;
+
+import com.sdm.ssm.account.model.vo.Account;
+
+public interface AccountService {
+
+	int countNull(Account account);
+	
+	int insertAccount(Account account);
+
+	int updateAccount(Account account);
+
+	int calCost(Account account);
+
+	int calTax(Account account);
+
+	int calRent(Account account);
+
+}
